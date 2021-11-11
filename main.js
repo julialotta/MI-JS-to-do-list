377679dc1bff139ae737165eb5fa57866a90f023
julialotta <julialottatinglof@gmail.com>
2021-11-11 17:18:12 +0100

    added sort by importance

--- main.js
+++ main.js
@@ -27,6 +27,29 @@ input.id = "todoinput";
 input.className ="todoinput";
 input.type ="text";
 input.placeholder = "write stuff here..."
+let labelforprio = document.createElement("label");
+labelforprio.for = "prioinput";
+labelforprio.innerHTML = "Add prio, 1-3";
+div.appendChild(labelforprio);
+let prio = document.createElement("select");
+div.appendChild(prio);
+prio.id = "prioinput";
+let priozero = document.createElement("option");
+priozero.innerHTML = "select importance";
+let prioone = document.createElement("option");
+prioone.innerHTML = "Very important";
+let priotwo = document.createElement("option");
+priotwo.innerHTML = "A little bit important";
+let priothree = document.createElement("option");
+priothree.innerHTML = "Not that important...";
+prioone.value = "1";
+priotwo.value = "2";
+priothree.value = "3";
+
+prio.appendChild(priozero);
+prio.appendChild(prioone);
+prio.appendChild(priotwo);
+prio.appendChild(priothree);
 
 let addbutton = document.createElement("button");
 div.appendChild(addbutton);
@@ -35,7 +58,7 @@ addbutton.id = "add";
 
 let sortbutton = document.createElement("button");
 div.appendChild(sortbutton);
-sortbutton.innerHTML= "Sort A-Z";
+sortbutton.innerHTML= "Sort by prio";
 sortbutton.id = "sort";
 
 let h2 = document.createElement("h2");
@@ -58,15 +81,16 @@ footerP.innerHTML = "This app is created by Julia-Lotta";
 //Skapandet av klasser
 
 class Todos {
-    constructor (task, checked) {
+    constructor (task, checked, prio) {
     this.task = task;
     this.checked = checked;
+    this.prio = prio;
     }
 }
 
-let task1 = new Todos ("Vara snäll", false);
-let task2 = new Todos ("Mata katten", false);
-let task3 = new Todos ("Koka kaffe", false);
+let task1 = new Todos ("Vara snäll", false, 1);
+let task2 = new Todos ("Mata katten", false, 1);
+let task3 = new Todos ("Koka kaffe", false, 1);
 
 let list = JSON.parse(localStorage.getItem("savedList")) || [task1, task2, task3];
 
@@ -101,6 +125,13 @@ function createHtml() {
         if (list[i].checked === true) {
             p.id = "checked";
         }
+        if (list[i].prio == 1) {
+            p.className = "prioone";
+        } if (list[i].prio == 2) {
+            p.className = "priotwo";
+        } if (list[i].prio == 3) {
+            p.className = "priothree";
+        }
         p.innerHTML += list[i].task;
         li.appendChild(cross);
         cross.addEventListener("click", () => {
@@ -126,20 +157,32 @@ updateLocalStorage();
 
 
 function addToList() {
-        if (input.value.length == 0) {
+        if ((input.value.length == 0) || (prio.value == "select importance") ) {
+            alert("Du har inte fyllt i korrekt");
         } else {
     let newTask = input.value;
     let listObject = new Todos (newTask);
+    let val = prio.value;
     listObject.checked = false; 
+
+    if (val == "1") {
+        listObject.prio = 1;
+    } if (val == "2") {
+         listObject.prio = 2;
+    } if (val == "3") {
+         listObject.prio = 3
+    }
+
     list.push(listObject);
     input.value = "";
+    prio.value = "";
     updateLocalStorage ();
 }
 }
 
 function sortList () {
    list.sort((a, b) => {
-      if (a.task.toLowerCase() > b.task.toLowerCase()) {
+      if (a.prio > b.prio) {
         return 1;
       } else {
         return -1;
@@ -158,4 +201,4 @@ function updateLocalStorage () {
 
 // att göra:
 // - strukturera i projeketet, selectors, event listeners och functios var för sig... 
-// Kunna sortera ordningen på dina todos
+// fixa design
